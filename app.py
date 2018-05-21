9bb9f4ae8215482a87f8bb935b3de4915834909f
wltsapp <wltsappstore@gmail.com>
2018-05-21 15:45:12 +0530

    add

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def upload():
     upfile = request.files["upfile"]
     img_file = "image" + str(random.randint(0, 999999)) + os.path.splitext(upfile.filename)[1]
     img_path = os.path.join("./cache/", img_file)         
-	if img_file != "":		
-		item = predictor.get_image_item(img_path) 
+	if '.mp4' in img_file:		
+		item = VideoPredictor.get_video_item(img_path) 
 	else:
 		item = predictor.get_image_item(img_path)   
 	upfile.save(img_path)		

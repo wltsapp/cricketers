c9e64f6539ddc648a285b08df82f9ec90b6e156d
wltsapp <wltsappstore@gmail.com>
2018-05-23 17:45:57 +0530

    Add

--- lib/video.py
+++ lib/video.py
@@ -60,7 +60,7 @@ class VideoPredictor:
 
 				print('Start Recognition')
 				prevTime = 0
-				while (video_capture.isOpened()):
+				while (True):
 					ret, frame = video_capture.read()
 					gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 					frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)

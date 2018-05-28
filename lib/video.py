582f2423ef281b6dddb8f039fe21d6877e71ac32
wltsapp <wltsappstore@gmail.com>
2018-05-28 10:14:17 +0530

    Add file

--- lib/video.py
+++ lib/video.py
@@ -63,7 +63,7 @@ class VideoPredictor:
 				while (True):
 					ret, frame = video_capture.read()
 					#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-					frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)
+					#frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)
 
 					curTime = time.time()+1    # calc fps
 					timeF = frame_interval
